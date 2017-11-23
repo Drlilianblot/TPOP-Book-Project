9fa1265312117ec502114d18112a23c7b4a43eff
Lilian Blot <lilian.blot@york.ac.uk>
2017-11-23 07:29:13 +0000

    ADDS PYTHON CODE FORMATING TO CHAPTER FUNCTIONS  - adds the chapters python file

---
+++ Python/Chapter_functions.py
@@ -0,0 +1,15 @@
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+def print_twice(word):
+    print(word)
+    print(word)
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
